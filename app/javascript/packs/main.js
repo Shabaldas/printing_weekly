94f4b4b941a4ee5aad4604e3f6da1354c06f5a81
ShevchukTania <taniabeley@ukr.net>
2020-05-17 19:57:15 +0300

    main page styles added

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -7,10 +7,9 @@ getLastIstagramPosts('renderweekly', 18)
       const image = document.createElement('img')
       const link = document.createElement('a')
       image.src = post.display_resources[0].src
-      image.style = 'width: 100px; height: 100px; object-fit:cover'
       link.href = `//www.instagram.com/p/${post.shortcode}`
       link.target = '_blank'
-      link.setAttribute('class', 'instagram-post')
+      link.setAttribute('class', 'instagram-post col-md-4 col-6')
       link.appendChild(image)
       return link
     }))
