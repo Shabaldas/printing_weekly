c36cb5904aa359024870c51b14570573f6fcf3be
shabaldas <shabaldasengr@gmail.com>
2020-07-09 01:16:41 +0300

    Remove bad url, and add new logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  # devise_for :users, ActiveAdmin::Devise.config
+  # ActiveAdmin.routes(self)
 
   root 'home#home'
 
   get 'home', to: 'home#home'
 
-  resources :blog_posts, only: %i[index show]
+  # resources :blog_posts, only: %i[index show]
 end
